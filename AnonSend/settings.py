67855823e9a73061b1af00a42dc543265cdb3551
apex-94 <Viperrko94>
2019-11-06 20:54:54 +0530

    roll back theme

--- AnonSend/settings.py
+++ AnonSend/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'che=x6jnu)1lq9xe=sy)jghx*t6w#qzh+nspccsix)@^l0@9z-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.anonsend.tech','34.93.47.115']
+ALLOWED_HOSTS = ['.anonsend.tech','34.93.47.115','127.0.0.1']
 
 # Application definition
 
